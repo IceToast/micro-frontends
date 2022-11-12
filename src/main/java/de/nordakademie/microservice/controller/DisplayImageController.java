60e696aea1b844665c7bf138290697921b8d3dff
IceToast <social@icetoast.cloud>
2022-11-12 12:06:00 +0100

    feat: Added DisplayImage Controller

---
+++ src/main/java/de/nordakademie/microservice/controller/DisplayImageController.java
@@ -0,0 +1,23 @@
+package de.nordakademie.microservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DisplayImageController {
+
+    @GetMapping(path = "/displayImage")
+    public ModelAndView fragment(@RequestParam String url, @RequestParam(required = false) Integer rotate) {
+        ModelAndView modelAndView = new ModelAndView("displayImage");
+            System.out.println(rotate);
+        if(rotate != null && rotate >= 0 && rotate < 360){
+            modelAndView.addObject("rotateDeg", rotate + "deg");
+        }
+        modelAndView.addObject("title", "Display Image");
+        modelAndView.addObject("imageUrl", url);
+        return modelAndView;
+    }
+
+}
